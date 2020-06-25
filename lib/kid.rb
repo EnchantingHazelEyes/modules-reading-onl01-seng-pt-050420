7a4edead2a13e0a7944ed78f237e090e2e269f14
Samantha Barros <samantha.barros95@gmail.com>
2020-06-25 04:08:19 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,14 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid 
-  include Dance 
-  extend MetaDancing
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
+  #include Dance 
+  #extend MetaDancing
   
   attr_accessor :name 
   
